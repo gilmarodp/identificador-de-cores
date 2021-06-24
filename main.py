6150dbf1c0a8b3bdd36c0fb535a85ccc257cdc27
Gilmar <42701487+gilmarodp@users.noreply.github.com>
2021-06-24 19:29:50 -0300

    separando constantes de cores em outro arquivo

--- main.py
+++ main.py
@@ -1,26 +1,8 @@
 import numpy as np
-import imutils
 import cv2
+import imutils
 
-# definindo constantes de intervalos de cores...
-LOWER = {
-    'red': (166, 84, 141),
-    'blue': (97, 100, 117),
-    'yellow': (23, 59, 119)
-}
-
-UPPER = {
-    'red': (186, 255, 255),
-    'blue': (117, 255, 255),
-    'yellow': (54, 255, 255)
-}
-
-# definindo cores dos contornos
-OUTLINE_COLORS = {
-    'red': (0, 0, 255),
-    'blue': (255, 0, 0),
-    'yellow': (0, 255, 217)
-}
+from src.colors import *
 
 camera = cv2.VideoCapture(0)
 
---
+++ src/colors.py
@@ -0,0 +1,19 @@
+# definindo constantes de intervalos de cores...
+LOWER = {
+    'red': (166, 84, 141),
+    'blue': (97, 100, 117),
+    'yellow': (23, 59, 119)
+}
+
+UPPER = {
+    'red': (186, 255, 255),
+    'blue': (117, 255, 255),
+    'yellow': (54, 255, 255)
+}
+
+# definindo cores dos contornos
+OUTLINE_COLORS = {
+    'red': (0, 0, 255),
+    'blue': (255, 0, 0),
+    'yellow': (0, 255, 217)
+}
